7b979352c4fb2b4372a2fdbd2e37926ecc4ebf1e
usa3amustafa <usa3a3ustafa@outlook.com>
2022-11-03 14:08:05 +0500

    changes

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const closeIcon = document.querySelector('.close-icon')
-const overlay = document.querySelector('.overlay')
-const cta = document.querySelectorAll('.cta')
-const content = document.querySelector('.content')
+const closeIcon = document.querySelector('.close-icon');
+const overlay = document.querySelector('#overlay');
+const cta = document.querySelectorAll('.cta');
+const content = document.querySelector('.content');
+
+overlay.style.display = 'none';
 
 closeIcon.addEventListener('click', () => {
-  content.style.display = 'block'
-  overlay.style.display = 'none'
-})
+  content.style.display = 'block';
+  overlay.style.display = 'none';
+});
 
 cta.forEach(cta =>
   cta.addEventListener('click', () => {
-    content.style.display = 'none'
-    overlay.style.display = 'flex'
+    content.style.display = 'none';
+    overlay.style.display = 'flex';
   })
-)
+);
